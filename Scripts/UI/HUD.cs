94367475119dbcc966f628f78313868bec661b30
danni <danni.allen@gmail.com>
2020-05-11 22:49:36 +1000

    add fps counter

--- Scripts/UI/HUD.cs
+++ Scripts/UI/HUD.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class HUD : CanvasLayer
 {
@@ -9,6 +10,8 @@ public class HUD : CanvasLayer
     public Sprite Crosshair;
     Control _manager;
     public Node2D AimAt;
+    List<uint> times = new List<uint>();
+    Label _fps;
 
     // Canvas layer currently has no visibility controls, parent a control node and use that for all other children
     private bool _visible = true;
@@ -27,6 +30,7 @@ public class HUD : CanvasLayer
         _armour = _manager.GetNode("ArmourLabel") as Label;
         Crosshair = _manager.GetNode("Crosshair") as Sprite;
         AimAt = _manager.GetNode("AimAt") as Node2D;
+        _fps = _manager.GetNode("FPS") as Label;
         this.Visible = false;
     }
 
@@ -41,6 +45,16 @@ public class HUD : CanvasLayer
         {
             _health.Text = Mathf.CeilToInt(_player.CurrentHealth).ToString();
             _armour.Text = Mathf.CeilToInt(_player.CurrentArmour).ToString();
+            
+            uint now = OS.GetTicksMsec();
+            while (times.Count > 0 && times[0] <= now - 1000)
+            {
+                times.RemoveAt(0);
+            }
+
+            times.Add(now);
+            _fps.Text = times.Count.ToString() + " FPS";
+
         }
     }   
 }
